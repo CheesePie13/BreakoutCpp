a6c1c009d2a753a981fa308583b7eeda92cd609d
CheesePie13 <>
2020-12-13 17:31:33 -0500

    - Move glad.c to third-party directory

--- mac-build-debug.sh
+++ mac-build-debug.sh
@@ -8,7 +8,7 @@ rm -r bin/mac-debug
 mkdir -p bin/mac-debug
 
 # Compile @todo: compile .c files with gcc
-g++ -o bin/mac-debug/BreakoutCppMac_debug.app src/*.cpp src/*.c -DMACOS -DVERSION=\"$version-debug\" -Iinclude -Llib-mac -lglfw3 -framework Cocoa -framework OpenGL -framework IOKit -Wall -O0 -g
+g++ -o bin/mac-debug/BreakoutCppMac_debug.app src/*.cpp src/third-party/*.c -DMACOS -DVERSION=\"$version-debug\" -Iinclude -Llib-mac -lglfw3 -framework Cocoa -framework OpenGL -framework IOKit -Wall -O0 -g
 
 # Copy shaders to bin/mac-debug
 cp -r assets/shaders bin/mac-debug/shaders
